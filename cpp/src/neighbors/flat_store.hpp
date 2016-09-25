98d4344f4e75a2660f5f80540653fcc083c0228a
D Blalock <dwb4ke@virginia.edu>
2016-09-25 17:56:18 -0400

    add row() accessor to RowArrays + better CRTP via type traits

--- cpp/src/neighbors/flat_store.hpp
+++ cpp/src/neighbors/flat_store.hpp
@@ -19,22 +19,13 @@
 
 namespace nn {
 
-
-
-
-
-// SELF: pick up by having the nn_index classes use (Fixed)RowArrays
-
-
-
-
 // ================================================================
 // Utils
 // ================================================================
 
-template<int AlignBytes, class IntT>
+template<class T, int AlignBytes, class IntT>
 inline IntT aligned_length(IntT ncols) {
-    int16_t align_elements = AlignBytes / sizeof(IntT);
+    int16_t align_elements = AlignBytes / sizeof(T);
     int16_t remainder = ncols % align_elements;
     if (remainder > 0) {
         ncols += align_elements - remainder;
@@ -42,7 +33,7 @@ inline IntT aligned_length(IntT ncols) {
     return ncols;
 }
 
-// helper struct to get Map<> MapOptions based on alignment in bytes
+// helper struct to get Eigen::Map<> MapOptions based on alignment in bytes
 template<int AlignBytes>
 struct _AlignHelper { enum { AlignmentType = Eigen::Unaligned }; };
 template<> struct _AlignHelper<16> { enum { AlignmentType = Eigen::Aligned }; };
@@ -65,36 +56,51 @@ static inline void aligned_free(T* p) {
 
 // ------------------------------------------------ traits
 
-// class RowArray;
-// class FixedRowArray;
+template<class T, int AlignBytes> class DynamicRowArray;
+template<class T, int NumCols, int AlignBytes> class FixedRowArray;
 
-// template<class Derived, class Args...>
-// struct row_array_traits;
+// set default param values here so don't have to pass them in
+template<class Derived, class T=float, int NumCols=-1, int AlignBytes=-1, class... Args>
+struct row_array_traits;
 
-// template <class Args...>
-// struct row_array_traits<RowArray> {
-//     typedef typedef Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> MatrixT;
-// };
+template <class T, int NumCols, int AlignBytes, class... Args>
+struct row_array_traits<DynamicRowArray<Args...>, T, NumCols, AlignBytes> {
+    typedef int32_t ColIndex;
+    typedef Eigen::Matrix<T, 1, Eigen::Dynamic, Eigen::RowMajor> RowT;
+    typedef Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> MatrixT;
+};
 
-// template <class T, int NumCols, class Args...>
-// struct row_array_traits<FixedRowArray> {
-//     typedef typedef Eigen::Matrix<T, Eigen::Dynamic, NumCols, Eigen::RowMajor> MatrixT;
-// };
+template <class T, int NumCols, int AlignBytes, class... Args>
+struct row_array_traits<FixedRowArray<T, NumCols, AlignBytes>, T, NumCols, AlignBytes, Args...> {
+    typedef int32_t ColIndex;
+    typedef Eigen::Matrix<T, 1, NumCols, Eigen::RowMajor> RowT;
+    typedef Eigen::Matrix<T, Eigen::Dynamic, NumCols, Eigen::RowMajor> MatrixT;
+};
 
 // ------------------------------------------------ BaseRowArray
 
+// BaseRowArray just has a pointer to its data and knows how wide each row
+// is by calling Derived::cols(). It can hand a pointer to the start of a
+// row, insert into a row, and copy one of its rows into another one; it
+// performs no bounds checks of any kind (and doesn't even know how long
+// its data is)
+//
+// Also aligns each row to AlignBytes boundaries, assuming that Derived::cols()
+// is guaranteed to yield a number of objects that's an even multiple of
+// AlignBytes / sizeof(T)
 template<class Derived, class T, int AlignBytes=32>
 class BaseRowArray {
 	typedef T Scalar;
-	// note: using a dynamic matrix here
-	typedef Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> MatrixT;
-	typedef Eigen::Map<MatrixT> MatrixMapT;
+    typedef typename row_array_traits<Derived, T>::MatrixT MatrixT;
+    typedef typename row_array_traits<Derived, T>::RowT RowT;
+	typedef Eigen::Map<MatrixT, _AlignHelper<AlignBytes>::AlignmentType> MatrixMapT;
+    typedef Eigen::Map<RowT, _AlignHelper<AlignBytes>::AlignmentType> RowMapT;
 
     static const bool IsRowMajor = true;
 
     // ------------------------ ctors
     BaseRowArray(size_t initial_rows, size_t ncols):
-        _data(aligned_alloc<Scalar>(initial_rows * aligned_length<AlignBytes>(ncols)))
+        _data(aligned_alloc<Scalar>(initial_rows * _aligned_length(ncols)))
     {
         assert(initial_rows >= 0);
     }
@@ -104,7 +110,7 @@ class BaseRowArray {
         BaseRowArray(X.rows(), X.cols())
     {
         assert(X.IsRowMajor);
-        auto ncols = aligned_length(X.cols());
+        auto ncols = _aligned_length(X.cols());
         MatrixMapT mat(_data, X.rows(), ncols);
         mat.topLeftCorner(X.rows(), X.cols()) = X;
     }
@@ -113,24 +119,35 @@ class BaseRowArray {
 
     // ------------------------ accessors
     template<class Index>
-    Scalar* row_ptr(const Index i) {
+    Scalar* row_ptr(const Index i) const {
         assert(i >= 0);
-		return _data + (i * Derived::cols());
+		return _data + (i * _cols());
     }
     template<class Index>
-    const Scalar* row_ptr(const Index i) { return row_ptr(i); }
+    const Scalar* row_ptr(const Index i) const { return row_ptr(i); }
+
+    template<class Index>
+    RowMapT row(const Index i) const {
+        return RowMapT{row_ptr(i), _cols()};
+    }
+
+    template<class Index>
+    MatrixMapT matrix(const Index nrows) const {
+        return MatrixMapT{_data, nrows, _cols()};
+    }
 
     // ------------------------ resize, insert, delete
     template<class Index>
     void resize(Index old_num_rows, Index new_num_rows) {
-        auto ncols = Derived::cols();
+        auto ncols = _cols();
+        assert (_aligned_length(ncols) == ncols);
+        assert(new_num_rows > old_num_rows); // for now, should only get bigger
+
         int64_t old_capacity = old_num_rows * ncols;
         int64_t new_capacity = new_num_rows * ncols;
-        // std::unique_ptr<Scalar[]> new_data(new Scalar[new_capacity]);
-        // std::copy(_data, _data + old_capacity, new_data.get());
-        // _data.swap(new_data);
+        int64_t len = std::min(old_capacity, new_capacity);
         Scalar* new_data = aligned_alloc<Scalar>(new_capacity);
-        std::copy(_data, _data + old_capacity, new_data);
+        std::copy(_data, _data + len, new_data);
         aligned_free(_data);
         _data = new_data;
     }
@@ -140,7 +157,7 @@ class BaseRowArray {
         auto row_start = row_ptr(at_idx);
         // zero past end of inserted data (and possibly a bit before)
         if (AlignBytes > 0 && NeedsZeroing) {
-            auto ncols = Derived::cols();
+            auto ncols = _cols();
             assert(AlignBytes > (sizeof(T) * ncols));
             auto row_end = row_start + ncols;
             auto zero_start = row_end - (AlignBytes / sizeof(T));
@@ -154,44 +171,43 @@ class BaseRowArray {
     template<class Index>
     void copy_row(Index from_idx, Index to_idx) {
         assert(from_idx != to_idx);
-		insert<Index, false>(row_ptr(from_idx), Derived::cols(), to_idx);
+		insert<Index, false>(row_ptr(from_idx), _cols(), to_idx);
     }
 
 protected:
     Scalar* _data;
+
+    size_t _aligned_length(size_t ncols) {
+        return aligned_length<T, AlignBytes>(ncols);
+    }
+private:
+    inline auto _cols() -> decltype(Derived::cols()) { return Derived::cols(); }
 };
 
-// ------------------------------------------------ RowArray
+// ------------------------------------------------ DynamicRowArray
 
+// BaseRowArray subclass with a width set at construction time
 template<class T, int AlignBytes=32>
-class RowArray : public BaseRowArray<RowArray<T, AlignBytes>, T, AlignBytes> {
-    typedef T Scalar;
-    typedef int32_t ColIndex;
-	typedef Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> MatrixT;
-    typedef Eigen::Map<MatrixT> MatrixMapT;
-	typedef BaseRowArray<RowArray<T, AlignBytes>, T, AlignBytes> Super;
+class DynamicRowArray : public BaseRowArray<DynamicRowArray<T, AlignBytes>, T, AlignBytes> {
+    // typedef int32_t ColIndex;
+    typedef typename row_array_traits<DynamicRowArray>::ColIndex ColIndex;
+	typedef BaseRowArray<DynamicRowArray<T, AlignBytes>, T, AlignBytes> Super;
 
     // ------------------------ ctors
     template<class Index>
-    RowArray(Index initial_rows, Index ncols):
-		Super(initial_rows, aligned_length<AlignBytes>(ncols)),
-        _ncols(aligned_length<AlignBytes>(ncols))
+    DynamicRowArray(Index initial_rows, Index ncols):
+		Super(initial_rows, _aligned_length(ncols)),
+        _ncols(_aligned_length(ncols))
     {
         assert(initial_rows >= 0);
     }
 
     // no copying
-    RowArray(const RowArray& other) = delete;
-    RowArray(const RowArray&& other) = delete;
+    DynamicRowArray(const DynamicRowArray& other) = delete;
+    DynamicRowArray(const DynamicRowArray&& other) = delete;
 
     // ------------------------ accessors
     ColIndex cols() const { return _ncols; }
-
-    template<class Index>
-    MatrixMapT matrix(Index nrows) {
-		MatrixMapT mat(Super::_data, nrows, _ncols);
-		return mat;
-    }
 
 private:
     ColIndex _ncols;
@@ -199,12 +215,11 @@ private:
 
 // ------------------------------------------------ FixedRowArray
 
+// BaseRowArray subclass that has a fixed width, and so doesn't need to
+// store it
 template<class T, int NumCols, int AlignBytes=32>
 class FixedRowArray : public BaseRowArray<FixedRowArray<T, NumCols, AlignBytes>, T, AlignBytes> {
-    typedef T Scalar;
-	typedef int32_t ColIndex;
-    typedef Eigen::Matrix<T, Eigen::Dynamic, NumCols, Eigen::RowMajor> MatrixT;
-    typedef Eigen::Map<MatrixT> MatrixMapT;
+	typedef typename row_array_traits<FixedRowArray>::ColIndex ColIndex;
     typedef BaseRowArray<FixedRowArray<T, NumCols, AlignBytes>, T, AlignBytes> Super;
 
     // ------------------------ ctors
@@ -213,12 +228,6 @@ class FixedRowArray : public BaseRowArray<FixedRowArray<T, NumCols, AlignBytes>,
 
     // ------------------------ accessors
     inline constexpr ColIndex cols() const { return NumCols; }
-
-    template<class Index>
-    MatrixMapT matrix(Index nrows) {
-        MatrixMapT mat(Super::_data, nrows, NumCols);
-        return mat;
-    }
 };
 
 // ================================================================
@@ -228,6 +237,9 @@ class FixedRowArray : public BaseRowArray<FixedRowArray<T, NumCols, AlignBytes>,
 // stores vectors as (optionally) aligned rows in a matrix; lets us access a
 // vector and know that it's 32-byte aligned, which enables using
 // 32-byte SIMD instructions (as found in 256-bit AVX 2)
+//
+// this code is somewhat redundant with RowArray code, but refactoring
+// to combine them is not a priority atm
 template<class T, int AlignBytes=32, class IdT=int64_t>
 class RowStore {
 public:
@@ -235,23 +247,17 @@ public:
     typedef T Scalar;
     typedef IdT Id;
     typedef int8_t PadLengthT;
-    // typedef Eigen::Map<Eigen::Matrix<Scalar, 1, -1>, Eigen::Aligned> RowT;
     typedef Eigen::Map<const Eigen::Matrix<Scalar, 1, Eigen::Dynamic>,
         _AlignHelper<AlignBytes>::AlignmentType> RowT;
-        // Eigen::Stride<Eigen::Dynamic, 1> > RowT;
     typedef Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> RowMatrixT;
-    // typedef Eigen::Map<RowMatrixT> DataMatrixT;
 
     static const bool IsRowMajor = true;
 
     // ------------------------ ctors
 
     RowStore(Index capacity, Index ncols):
-        _pad_width(static_cast<PadLengthT>(aligned_length<AlignBytes>(ncols) - ncols)),
-        _data(capacity, aligned_length<AlignBytes>(ncols))
-//        _ncols(aligned_length<AlignBytes>(ncols)),
-        // _capacity(capacity),
-//        _data(new Scalar[capacity * _ncols]())
+        _pad_width(static_cast<PadLengthT>(aligned_length<Scalar, AlignBytes>(ncols) - ncols)),
+        _data(capacity, aligned_length<Scalar, AlignBytes>(ncols))
     {
         assert(capacity > 0);
         if (_pad_width > 0) {
@@ -265,18 +271,6 @@ public:
         _data.topLeftCorner(X.rows(), X.cols()) = X;
         _ids = ar::range(static_cast<Id>(0), static_cast<Id>(X.rows()));
     }
-
-    // template<class RowMatrixT>
-    // RowStore(const RowMatrixT&& X) noexcept : _data(X) {}
-        // _ncols(aligned_length<AlignBytes>(X.cols())),
-        // _capacity(X.rows()),
-        // _data(new Scalar[_capacity * _ncols]())
-    // {
-        // assert(_capacity > 0);
-        // for (size_t i = 0; i < X.rows(); i++) {
-        //     insert(X.row(i).data(), i);
-        // }
-    // }
 
     // ------------------------ accessors
     Index rows() const { return _ids.size(); }
@@ -313,17 +307,10 @@ public:
         auto cap = capacity_rows();
         Index ret = 0;
         if (rows() > cap) { // resize if needed
-            // Index new_size = std::max(capacity + 1, _capacity * 1.5) * _ncols;
-            // std::unique_ptr<Scalar[]> new_data(new Scalar[new_capacity]);
-            // std::copy(_data, _data + size(), new_data.get());
-            // _capacity = new_capacity;
-            // _data.swap(new_data);
             Index new_capacity = std::max(cap + 1, cap * 1.5);
             _data.conservativeResize(new_capacity, Eigen::NoChange);
             ret = new_capacity;
         }
-//      _data.row(rows()) =
-//        Scalar* row_start_ptr = _data + (_ncols * rows());
         Scalar* row_start_ptr = row_ptr(rows());
         std::copy(row_start, row_start + (cols() - _pad_width), row_start_ptr);
         if (AlignBytes > 0 && _pad_width > 0) { // check alignbytes to short-circuit
@@ -339,26 +326,15 @@ public:
         if (idx < 0) {
             return -1;
         }
-        // move data at the end to overwrite the removed row so that
-        // everything remains contiguous
-        // auto last_idx = _ids.size() - 1;
-        // _ids[idx] = _ids[last_idx];
-        // auto last_row_start = row_ptr(last_idx);
-        // Scalar* replace_row_ptr = row_ptr(idx);
-        // std::copy(last_row_start, last_row_start + _ncols, replace_row_ptr);
-
         _ids[idx] = _ids.back();
         _ids.pop_back(); // reduce size
         return idx;
     }
 
 private:
-    // int32_t _ncols; // 4B
-    // int32_t _capacity; // 4B
     std::vector<Id> _ids; // 24B; size() = number of entries
     RowMatrixT _data; // 24B; rows, cols = capacity, vector length
     PadLengthT _pad_width; // 1B
-    // std::unique_ptr<Scalar[]> _data; // 8B
 };
 
 } // namespace nn
