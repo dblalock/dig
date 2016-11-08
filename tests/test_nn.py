b68b5db70623a2d5b42a3d18db8a269aa546aadd
D Blalock <dwb4ke@virginia.edu>
2016-11-07 23:37:50 -0500

    update tests to include sift + glove + more random data

--- tests/test_nn.py
+++ tests/test_nn.py
@@ -1,10 +1,14 @@
 #!/bin/env python
 
-import functools
-import time
+# import functools
+import os
 import numpy as np
+import time
 
 import dig
+
+from joblib import Memory
+_memory = Memory('.', verbose=1)
 
 
 def all_eq(x, y):
@@ -140,13 +144,15 @@ def test_index(index=dig.MatmulIndex, name="cpp", N=100, D=100, r2=-1,
 
     # ------------------------ knn query
 
+    t0 = time.clock()
     nn10 = index.knn(q, 10, **search_kwargs)
+    t_py = (time.clock() - t0) * 1000
     t = index.getQueryTimeMs()
     trueNN10 = np.argsort(trueDists)[:10]
     # print "nn10, true nn10 =\n{}\n{}".format(nn10, trueNN10)
     # print "sorted nn10, true nn10 =\n{}\n{}".format(
     #     sorted(nn10), sorted(trueNN10))
-    print "-> {} knn time\t\t= {}".format(name, t)
+    print "-> {} knn time\t\t= {} ({:.3} py)".format(name, t, t_py)
     assert(no_asserts or all_eq(nn10, trueNN10))
 
     knn_dists = trueDists[nn10]
@@ -199,31 +205,137 @@ def debug():
     index.radius(q, 99.)
 
 
+class Datasets:
+    RAND_UNIF = 0
+    RAND_GAUSS = 1
+    RAND_WALK = 2
+    GLOVE = 10
+    GLOVE_100 = 11
+    GLOVE_200 = 12
+    SIFT = 20
+    SIFT_100 = 21
+    SIFT_200 = 22
+
+    RAND_DATASETS = [RAND_UNIF, RAND_GAUSS, RAND_WALK]
+    FILE_DATASETS = [GLOVE, SIFT, GLOVE_100, SIFT_100, GLOVE_200, SIFT_200]
+
+
+class Paths:
+    DATA_DIR = os.path.expanduser('~/Desktop/datasets/nn-search')
+    GLOVE = os.path.join(DATA_DIR, 'glove.txt')
+    GLOVE_100 = os.path.join(DATA_DIR, 'glove_100k.txt')
+    GLOVE_200 = os.path.join(DATA_DIR, 'glove_200k.txt')
+    SIFT = os.path.join(DATA_DIR, 'sift.txt')
+    SIFT_100 = os.path.join(DATA_DIR, 'sift_100k.txt')
+    SIFT_200 = os.path.join(DATA_DIR, 'sift_200k.txt')
+
+
+@_memory.cache  # cache this more efficiently than as text
+def cached_load_txt(*args, **kwargs):
+    return np.loadtxt(*args, **kwargs)
+
+
+def load_dataset(which_dataset, N=-1, D=-1, norm_mean=False, norm_len=False):
+    if which_dataset == Datasets.RAND_UNIF:
+        X = np.random.rand(N, D)
+        q = np.random.rand(D)
+    elif which_dataset == Datasets.RAND_GAUSS:
+        X = np.random.randn(N, D)
+        q = np.random.randn(D)
+    elif which_dataset == Datasets.RAND_WALK:
+        X = np.random.randn(N, D)
+        X = np.cumsum(X, axis=1)
+        q = np.random.randn(D)
+        q = np.cumsum(q)
+    elif which_dataset == Datasets.GLOVE:
+        X = cached_load_txt(Paths.GLOVE)
+    elif which_dataset == Datasets.SIFT:
+        X = cached_load_txt(Paths.SIFT)
+    elif which_dataset == Datasets.GLOVE_100:
+        X = cached_load_txt(Paths.GLOVE_100)
+    elif which_dataset == Datasets.SIFT_100:
+        X = cached_load_txt(Paths.SIFT_100)
+    elif which_dataset == Datasets.GLOVE_200:
+        X = cached_load_txt(Paths.GLOVE_200)
+    elif which_dataset == Datasets.SIFT_200:
+        X = cached_load_txt(Paths.SIFT_200)
+    else:
+        raise ValueError("unrecognized dataset {}".format(which_dataset))
+
+    if which_dataset in Datasets.FILE_DATASETS:
+        if N > 0 and N < X.shape[0]:
+            X = X[:N, :]
+            q = X[N]
+        else:
+            X = X[:-1, :]
+            # q = np.copy(X[-1])
+            # q = np.random.randn(X.shape[1])
+            idx = np.random.randint(X.shape[1] - 1)
+            q = np.copy(X[idx])
+            q[64:] = X[idx + 1, 64:]  # mix 2 examples so differs from both
+            print "start of q: ", q[:10]
+
+    if norm_mean:
+        means = np.mean(X, axis=0)
+        X -= means
+        q -= means
+    if norm_len:
+        X /= np.linalg.norm(X, axis=1, keepdims=True)
+        q /= np.linalg.norm(q)
+
+    return X.astype(np.float32), q.astype(np.float32)
+
+
 if __name__ == '__main__':
     # debug()
 
     # N = 1000
     # N = 10 * 1000
-    # N = 100 * 1000
+    N = 100 * 1000
     # N = 500 * 1000
-    N = 1000 * 1000
-    D = 100
+    # N = 1000 * 1000
+    D = 128
     # D = 200
 
-    X = np.random.randn(N, D)
-    X = np.cumsum(X, axis=1)
+    # X = load_dataset(Datasets.RAND_WALK, N, D, norm_len=True)  # 1.002
+    # X = load_dataset(Datasets.RAND_UNIF, N, D, norm_len=True)  # 1.002
+    # X = load_dataset(Datasets.RAND_GAUSS, N, D, norm_len=True)  # 1.03
+    # X = load_dataset(Datasets.RAND_GAUSS, N, D, norm_mean=True)  # 1.03
+    # X, q = load_dataset(Datasets.GLOVE_100, norm_mean=True)  # 2.5ish?
+    # X, q = load_dataset(Datasets.SIFT_100, norm_mean=True)  # 5ish?
+    # X, q = load_dataset(Datasets.GLOVE_200, norm_mean=True)  #
+    # X, q = load_dataset(Datasets.SIFT_200, norm_mean=True)  #
+    X, q = load_dataset(Datasets.GLOVE, norm_mean=True)  #
+    # X, q = load_dataset(Datasets.SIFT, norm_mean=True)  #
+
+    # so 5% seems to get it 80-100% of the top 10nn right basically all of
+    # the time in about .5-.6ms using 200k pts. Below that acc becomes really
+    # variable and much lower.
+    # Also, note that I accidentally did these with 316 (sqrt(100k)) centroids,
+    # so not certain yet what happens when there are more of them
+
+    # print "nan idxs: ", np.where(np.isnan(X))[0]
+    # print "inf idxs: ", np.where(np.isinf(X))[0]
+
+    # import sys; sys.exit()
+
+    # X = np.random.randn(N, D)
+    # X = np.cumsum(X, axis=1)
     # X -= np.mean(X, axis=1, keepdims=True) # rel contrast of 1.2
-    X /= np.std(X, axis=1, keepdims=True)  # rel contrast of ~4
-    Xfloat = X.astype(np.float32)
+    # X /= np.std(X, axis=1, keepdims=True)  # rel contrast of ~4
 
-    q = np.cumsum(np.random.randn(D))
-    qfloat = q.astype(np.float32)
+    # Xfloat = X.astype(np.float32)
 
-    t0 = time.clock()
-    diffs = X - q
-    trueDists = np.sum(diffs * diffs, axis=1)
-    t_python = (time.clock() - t0) * 1000
-    print "-> python full dists time double\t= {}".format(t_python)
+    # q = np.cumsum(np.random.randn(D))
+    # qfloat = q.astype(np.float32)
+    Xfloat = X
+    qfloat = q
+
+    # t0 = time.clock()
+    # diffs = X - q
+    # trueDists = np.sum(diffs * diffs, axis=1)
+    # t_python = (time.clock() - t0) * 1000
+    # print "-> python full dists time double\t= {}".format(t_python)
 
     t0 = time.clock()
     diffs = Xfloat - qfloat
@@ -233,10 +345,14 @@ if __name__ == '__main__':
 
     minDist = np.min(trueDistsF)
     avgDist = np.mean(trueDistsF)
+    print "avg dist: ", avgDist
+    print "min dist: ", minDist
     print "relative contrast: ", avgDist / minDist
 
-    opts_dbl = dict(X=X, q=q, dtype=None, trueDists=trueDists)
+    opts_dbl = dict(X=X, q=q, dtype=None, trueDists=trueDistsF)
     opts_flt = dict(X=Xfloat, q=qfloat, dtype=np.float32, trueDists=trueDistsF)
+
+    # import sys; sys.exit()
 
     # test_index(dig.MatmulIndex, "matmul", **opts_dbl)
     # test_index(dig.MatmulIndexF, "matmulf", **opts_flt)
@@ -264,8 +380,10 @@ if __name__ == '__main__':
     run_dbl = False
     run_flt = True
 
-    two_level = True
+    # two_level = True
+    two_level = False
 
+    N = X.shape[0]
     k = int(np.power(N, 1./3)) if two_level else int(np.sqrt(N))
     print "building index with {} centroids...".format(k)
 
