add guassian rotation init for OPQ + fix failure to use rotation in update

tests/quantize.py
@@ -47,7 +47,7 @@ def learn_pq(X, ncentroids, nsubvects, subvect_len, max_kmeans_iters=16):
     codebooks = np.empty((ncentroids, nsubvects, subvect_len))
     assignments = np.empty((X.shape[0], nsubvects), dtype=np.int)
 
-    print "codebooks shape: ", codebooks.shape
+    # print "codebooks shape: ", codebooks.shape
 
     for i in range(nsubvects):
         start_col = i * subvect_len
@@ -104,6 +104,84 @@ def _encode_X_pq(X, codebooks, elemwise_dist_func=_dists_elemwise_sq):
     return idxs  # [N x nsubvects]
 
 
+# ================================================================ Gaussian OPQ
+
+# https://github.com/yahoo/lopq/blob/master/python/lopq/model.py; see
+# https://github.com/yahoo/lopq/blob/master/LICENSE. For this function only:
+#
+# Copyright 2015, Yahoo Inc.
+# Licensed under the terms of the Apache License, Version 2.0.
+# See the LICENSE file associated with the project for terms.
+#
+def eigenvalue_allocation(num_buckets, eigenvalues):
+    """
+    Compute a permutation of eigenvalues to balance variance accross buckets
+    of dimensions.
+    Described in section 3.2.4 in http://research.microsoft.com/pubs/187499/cvpr13opq.pdf
+    Note, the following slides indicate this function will break when fed eigenvalues < 1
+    without the scaling trick implemented below:
+        https://www.robots.ox.ac.uk/~vgg/rg/slides/ge__cvpr2013__optimizedpq.pdf
+    :param int num_buckets:
+        the number of dimension buckets over which to allocate eigenvalues
+    :param ndarray eigenvalues:
+        a vector of eigenvalues
+    :returns ndarray:
+        a vector of indices by which to permute the eigenvectors
+    """
+    D = len(eigenvalues)
+    dims_per_bucket = D / num_buckets
+    eigenvalue_product = np.zeros(num_buckets, dtype=float)
+    bucket_size = np.zeros(num_buckets, dtype=int)
+    permutation = np.zeros((num_buckets, dims_per_bucket), dtype=int)
+
+    # We first must scale the eigenvalues by dividing by their
+    # smallets non-zero value to avoid problems with the algorithm
+    # when eigenvalues are less than 1.
+    min_non_zero_eigenvalue = np.min(np.abs(eigenvalues[np.nonzero(eigenvalues)]))
+    eigenvalues = eigenvalues / min_non_zero_eigenvalue
+
+    # Iterate eigenvalues in descending order
+    sorted_inds = np.argsort(eigenvalues)[::-1]
+    log_eigs = np.log2(abs(eigenvalues))
+    for ind in sorted_inds:
+
+        # Find eligible (not full) buckets
+        eligible = (bucket_size < dims_per_bucket).nonzero()
+
+        # Find eligible bucket with least eigenvalue product
+        i = eigenvalue_product[eligible].argmin(0)
+        bucket = eligible[0][i]
+
+        # Update eigenvalue product for this bucket
+        eigenvalue_product[bucket] = eigenvalue_product[bucket] + log_eigs[ind]
+
+        # Store bucket assignment and update size
+        permutation[bucket, bucket_size[bucket]] = ind
+        bucket_size[bucket] += 1
+
+    return np.reshape(permutation, D)
+
+
+def learn_opq_gaussian_rotation(X_train, ncodebooks, codebook_bits):
+    means = np.mean(X_train, axis=0)
+    # X = X_train - means
+    cov = np.dot(X_train.T, X_train) - np.outer(means, means)
+    eigenvals, eigenvects = np.linalg.eigh(cov)
+
+    # tmp = np.dot(eigenvects.T, eigenvects)
+    # I = np.identity(X_train.shape[1], dtype=np.float32)
+    # assert np.max(np.abs(tmp - I)) < .001
+
+    # return np.identity(X_train.shape[1], dtype=np.float32)
+    # return np.identity(X_train.shape[1])
+    # return eigenvects  # TODO rm after debug
+
+    order_idxs = eigenvalue_allocation(ncodebooks, eigenvals)
+    assert len(order_idxs) == X_train.shape[1]
+    # print order_idxs
+    return eigenvects[:, order_idxs].T  # rows are projections
+
+
 # ================================================================ OPQ
 
 def _update_centroids_opq(X, assignments, ncentroids):  # [N x D], [N x M]
@@ -155,10 +233,11 @@ def opq_rotate(X, R):  # so other code need not know what to transpose
     return np.dot(np.atleast_2d(X), R.T)
 
 
-# based on https://github.com/arbabenko/Quantizations/blob/master/opqCoding.py
+# loosely based on:
+# https://github.com/arbabenko/Quantizations/blob/master/opqCoding.py
 # @_memory.cache
 def learn_opq(X_train, ncodebooks, codebook_bits=8, niters=20,
-              initial_kmeans_iters=1, debug=False):
+              initial_kmeans_iters=1, init_via_gauss=True, debug=False):
 
     X = X_train.astype(np.float32)
     N, D = X.shape
@@ -170,12 +249,25 @@ def learn_opq(X_train, ncodebooks, codebook_bits=8, niters=20,
 
     assert D % subvect_len == 0  # equal number of dims for each codebook
 
-    R = np.identity(D, dtype=np.float32)  # D x D
-    # X_rotated = np.dot(X, R.T)  # (N x D) * (D x D) = N x D
+    if init_via_gauss:
+        R = learn_opq_gaussian_rotation(X_train, ncodebooks, codebook_bits)
+        R = R.astype(np.float32)
+        # _debug_rotation(R)
+        X_rotated = opq_rotate(X, R)
+        assert X.shape[1] == R.shape[0]
+        assert X.shape[1] == R.shape[1]
+        assert X.shape == X_rotated.shape
+        norms = np.linalg.norm(X, axis=1)
+        norms_rot = np.linalg.norm(X_rotated, axis=1)
+        # print "orig norms, rotated norms: ", norms, norms_rot
+        assert np.max(np.abs(norms - norms_rot)) < .01
+    else:
+        R = np.identity(D, dtype=np.float32)  # D x D
+        X_rotated = X
+        # X_rotated = opq_rotate(X, R)
 
     # initialize codebooks by running kmeans on each rotated dim; this way,
     # setting niters=0 corresponds to normal PQ
-    X_rotated = X
     codebooks, assignments = learn_pq(X_rotated, ncentroids=ncentroids,
                                       nsubvects=ncodebooks,
                                       subvect_len=subvect_len,
@@ -189,9 +281,9 @@ def learn_opq(X_train, ncodebooks, codebook_bits=8, niters=20,
     #     end_col = start_col + subvect_len
     #     codebooks[:, m, :] = X_rotated[rand_idxs, start_col:end_col]
 
-    prev_err = np.inf
-    prev_codebooks = None
-    prev_assignments = None
+    # prev_err = np.inf
+    # prev_codebooks = None
+    # prev_assignments = None
     for it in np.arange(niters):
         # compute reconstruction errors
         X_hat = reconstruct_X_pq(assignments, codebooks)
@@ -199,33 +291,37 @@ def learn_opq(X_train, ncodebooks, codebook_bits=8, niters=20,
         err = np.mean(errors * errors) / normalize_mse_by
         print "---- OPQ iter {}: mse / variance = {}".format(it, err)
 
-        if err > prev_err:
-            print "WARNING: OPQ began to diverge"
-            try:
-                _debug_rotation(R)
-            except NumericalException:
-                pass
-            codebooks = prev_codebooks
-            assignments = prev_assignments
-            break  # computation is diverging
-        prev_err = err
+        # if err > prev_err:
+        #     print "WARNING: OPQ began to diverge"
+        #     try:
+        #         _debug_rotation(R)
+        #     except NumericalException:
+        #         pass
+        #     codebooks = prev_codebooks
+        #     assignments = prev_assignments
+        #     break  # computation is diverging
+        # prev_err = err
 
         # update rotation matrix based on reconstruction errors
         U, s, V = np.linalg.svd(np.dot(X_hat.T, X), full_matrices=False)
         R = np.dot(U, V)
 
-        if debug:
-            try:
-                _debug_rotation(R)
-            except NumericalException as e:
-                raise e
+        # if debug:
+        #     try:
+        #         _debug_rotation(R)
+        #     except NumericalException as e:
+        #         raise e
 
         # update centroids using new rotation matrix
+        # prev_codebooks = codebooks
+        # prev_assignments = assignments
         X_rotated = opq_rotate(X, R)
-        prev_codebooks = codebooks
-        prev_assignments = assignments
-        codebooks = _update_centroids_opq(X, assignments, ncentroids)
         assignments = _encode_X_pq(X_rotated, codebooks)
+        # X_hat = reconstruct_X_pq(assignments, codebooks)
+        # errors = X_rotated - X_hat
+        # err = np.mean(errors * errors) / normalize_mse_by
+        # print "---- part b OPQ iter {}: mse / variance = {}".format(it, err)
+        codebooks = _update_centroids_opq(X_rotated, assignments, ncentroids)
 
     X_hat = reconstruct_X_pq(assignments, codebooks)
     errors = X_rotated - X_hat
@@ -240,24 +336,27 @@ def main():
     # tmp = datasets.load_dataset(
     X_train, Q, X_test, truth = datasets.load_dataset(
         # datasets.Random.UNIFORM, N=1000, D=64)
-        # datasets.Glove.TEST_100, N=10000)
+        datasets.Glove.TEST_100, N=10000, D=32)
         # datasets.Glove.TEST_100, N=100000)
-        # datasets.Sift1M.TEST_100, N=100000)
+        # datasets.Sift1M.TEST_100, N=10000, D=32)
         # datasets.Gist.TEST_100, N=50000)
         # datasets.Glove.TEST_100, D=96)
-        datasets.Random.BLOBS, N=10000, D=96)
+        # datasets.Random.BLOBS, N=10000, D=32)
 
     # print X_train.shape
 
     # codebooks, assignments, R = learn_opq(X_train, ncodebooks=4, niters=0)
     # codebooks, assignments, R = learn_opq(X_train, ncodebooks=4, niters=2)
     # codebooks, assignments, R = learn_opq(X_train, ncodebooks=4, niters=20, debug=True)
-    codebooks, assignments, R = learn_opq(X_train, ncodebooks=4, niters=20)
+    # codebooks, assignments, R = learn_opq(X_train, ncodebooks=4, niters=20)
     # codebooks, assignments, R = learn_opq(X_train, ncodebooks=8, niters=20)
 
     # codebooks, assignments, R = learn_opq(X_train, ncodebooks=4, niters=0,
     #                                       initial_kmeans_iters=16)
 
+    niters = 5
+    codebooks, assignments, R = learn_opq(X_train, ncodebooks=4, niters=niters, init_via_gauss=False)
+    codebooks, assignments, R = learn_opq(X_train, ncodebooks=4, niters=niters)
 
 
 if __name__ == '__main__':