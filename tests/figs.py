2febbc34be5f86353720d408d280561bb72f3e7c
D Blalock <dwb4ke@virginia.edu>
2017-02-13 19:28:54 -0500

    add code to generate more figs + tweak encoding speed fig

--- tests/figs.py
+++ tests/figs.py
@@ -10,12 +10,23 @@ SAVE_DIR = os.path.expanduser('~/Desktop/bolt/figs/')
 
 
 def save_fig(name):
-    plt.savefig(os.path.join(SAVE_DIR, name + '.pdf'))
+    plt.savefig(os.path.join(SAVE_DIR, name + '.pdf'), bbox_inches='tight')
 
 
-def set_style():
+def set_style():  # not using this atm, but cool that it's possible
     plt.style.use(['seaborn-white', 'seaborn-paper'])
     matplotlib.rc("font", family="Times New Roman")
+
+
+def set_palette(ncolors=8):  # use this to change color palette in all plots
+    pal = sb.color_palette("Set1", n_colors=8)
+    # pal2 = sb.xkcd_palette(["amber"])
+    # # pal2 = sb.color_palette(["yellow"])
+    # pal[5] = pal[0]
+    # pal[0] = pal2[0]
+    sb.set_palette(pal)
+
+    return pal
 
 
 def popcount_fig():
@@ -34,10 +45,11 @@ def popcount_fig():
     # popcnt_512d_32b = np.random.randn(10) + 2 / 2
 
     # sb.set_context("poster", rc={"figure.figsize": (8, 4)})
-    sb.set_context("talk", rc={"figure.figsize": (6, 4)})
+    sb.set_context("talk")
+    set_palette(ncolors=2)
+    _, ax = plt.subplots(1, figsize=(6, 4))
 
     # sb.set_palette("Set1", n_colors=2)
-    set_palette(ncolors=2)
 
     # fig, axes = plt.subplots(1, 2)
 
@@ -54,7 +66,7 @@ def popcount_fig():
     # df.rename(columns={'algo': 'Algorithm'}, inplace=True)
     df.rename(columns={'algo': ' '}, inplace=True)  # hide from legend
 
-    ax = sb.barplot(x='x', y='t', hue=' ', ci=95, data=df)
+    sb.barplot(x='x', y='t', hue=' ', ci=95, data=df, ax=ax)
     ax.set_title('Distance Computations Per Second')
     ax.set_xlabel('Encoding Length (Bytes)')
     ax.set_ylabel('Millions of distances / sec')
@@ -64,7 +76,7 @@ def popcount_fig():
     # plt.show()
 
 
-def encoding_fig(data_enc=True):
+def encoding_fig(data_enc=True, data=None):
     # sb.set_context("talk", rc={"figure.figsize": (6, 6)})
     sb.set_context("talk", rc={"figure.figsize": (7, 7)})
     # sb.set_context("talk", rc={"figure.figsize": (8, 8)})
@@ -81,22 +93,18 @@ def encoding_fig(data_enc=True):
     # sb.set_palette("Set1", n_colors=len(ALGOS))
     set_palette(ncolors=len(ALGOS))
 
-    condition = ALGOS
+    fake_data = data is None
+    if fake_data:
+        data = np.random.randn(1, len(lengths), len(algo2offset))
+        for i, algo in enumerate(ALGOS):
+            data[:, :, i] += algo2offset[algo]
+        data /= np.arange(len(lengths)).reshape((1, -1, 1))
 
-    # prefix = 'Data' if data_enc else 'Query'
+    # condition = ALGOS
 
     # ================================ data encoding
 
-    # prefix = 'Data'
-
     # ------------------------ 8B encodings
-
-    data = np.random.randn(1, len(lengths), len(algo2offset))
-    for i, algo in enumerate(ALGOS):
-        data[:, :, i] += algo2offset[algo]
-    data /= np.arange(len(lengths)).reshape((1, -1, 1))
-    # if not data_enc:  # query encoding
-    #     data += np.random.randn(*data.shape) * 5
 
     ax = axes[0, 0]
     # sb.tsplot(data=data, condition=condition, time=lengths, ax=ax)
@@ -105,22 +113,16 @@ def encoding_fig(data_enc=True):
     ax.set_title('Data Encoding Speed', y=1.02)
 
     # ------------------------ 16B encodings
-
     data /= 2
     ax = axes[1, 0]
     sb.tsplot(data=data, condition=None, time=lengths, ax=ax)
-    # ax.set_title(prefix + ' Encoding Speed, 16B codes')
 
     # ------------------------ 32B encodings
-
     data /= 2
     ax = axes[2, 0]
     sb.tsplot(data=data, condition=None, time=lengths, ax=ax)
-    # ax.set_title(prefix + ' Encoding Speed, 32B codes')
 
     # ================================ query encoding
-
-    # prefix = 'Query'
 
     # fake data
     data *= 8
@@ -129,30 +131,38 @@ def encoding_fig(data_enc=True):
     # ------------------------ 8B encodings
 
     ax = axes[0, 1]
-    sb.tsplot(data=data, condition=condition, time=lengths, ax=ax)
+    sb.tsplot(data=data, condition=None, time=lengths, ax=ax)
     # ax.set_title(prefix + ' Encoding Speed')
-    ax.set_title('Query Encoding Speed', y=1.02)
+    ax.set_title('Query Encoding Speed', y=1.03, fontsize=16)
 
     # ------------------------ 16B encodings
 
     data /= 2
     ax = axes[1, 1]
     sb.tsplot(data=data, condition=None, time=lengths, ax=ax)
-    # ax.set_title(prefix + ' Encoding Speed, 16B codes')
 
     # ------------------------ 32B encodings
 
     data /= 2
     ax = axes[2, 1]
-    sb.tsplot(data=data, condition=None, time=lengths, ax=ax)
-    # ax.set_title(prefix + ' Encoding Speed, 32B codes')
+    sb.tsplot(data=data, condition=ALGOS, time=lengths, ax=ax)
+
+    # ------------------------ legend
+
+    ax = axes.ravel()[-1]
+    leg_lines, leg_labels = ax.get_legend_handles_labels()
+    ax.legend_.remove()
+    # leg_lines, leg_labels = leg_lines[:len(ALGOS)], leg_labels[:len(ALGOS)]
+
+    plt.figlegend(leg_lines, leg_labels, loc='lower center',
+                  ncol=len(ALGOS), labelspacing=0)
 
     # ------------------------ postproc + save plot
 
     for ax in axes[-1, :].ravel():
-        ax.set_xlabel('Vector length')
+        ax.set_xlabel('Vector Length')
     for ax in axes[:, 0]:
-        ax.set_ylabel('Million vectors / s')
+        ax.set_ylabel('Million Vectors / s')
 
     # only bottom row gets xlabels
     for ax in axes[:2, :].ravel():
@@ -165,6 +175,7 @@ def encoding_fig(data_enc=True):
         ax.set_ylabel(fmt_str.format((2 ** i) * 8), labelpad=10, fontsize=15)
 
     plt.tight_layout()
+    plt.subplots_adjust(bottom=.15)
     save_fig('encoding_speed')
     # plt.show()
     # if data_enc:
@@ -242,7 +253,6 @@ def query_speed_fig():
 
     # add byte counts on the right
     sb.set_style("white")  # adds border (spines) we have to remove
-    # sb.set_style("ticks")
     fmt_str = "{}B Encodings"
     for i, ax in enumerate(axes):
         ax2 = ax.twinx()
@@ -273,15 +283,213 @@ def query_speed_fig():
     # plt.show()
 
 
-def set_palette(ncolors=8):
-    pal = sb.color_palette("Set1", n_colors=8)
-    # pal2 = sb.xkcd_palette(["amber"])
-    # # pal2 = sb.color_palette(["yellow"])
-    # pal[5] = pal[0]
-    # pal[0] = pal2[0]
-    sb.set_palette(pal)
+def recall_r_fig(data=None, suptitle=None, fname='l2_recall'):
+    # experiment params:
+    #   datasets = Sift1M, Convnet1M, LabelMe22k, MNIST
+    #   bytes = [8, 16, 32]
+    #   R = 1, 10, 100, 1000
 
-    return pal
+    DATASETS = ['Sift1M', 'Convnet', 'LabelMe', 'MNIST']
+    ALGOS = ['Bolt', 'PQ', 'OPQ', 'PairQ']
+    NBYTES_LIST = [8, 16, 32]
+    Rs = [1, 10, 100, 1000]
+
+    if suptitle is None:
+        suptitle = 'Nearest Neighbor Recall'
+
+    fake_data = data is None
+    if fake_data:
+        algo2offset = {'Bolt': -.1, 'PQ': -.2, 'OPQ': 0, 'PairQ': .1}
+        data = np.random.rand(1, len(Rs), len(algo2offset))
+        data = np.sort(data, axis=1)  # ensure fake recalls are monotonic
+        for i, algo in enumerate(ALGOS):
+            recall = data[:, :, i] + algo2offset[algo]
+            data[:, :, i] = np.clip(recall, 0., 1.)
+
+    # sb.set_context("talk", rc={"figure.figsize": (6, 8)})
+    sb.set_context("talk", rc={"figure.figsize": (6, 9)})
+    # fig, axes = plt.subplots(2, 2)
+    # fig, axes = plt.subplots(4, 1)
+    # axes = axes.reshape((4, 1))
+    fig, axes = plt.subplots(4, 3)
+
+    # line_styles_for_nbytes = {8: '--', 16: '-', 32: '.-'}
+    line_styles_for_nbytes = {8: '-', 16: '-', 32: '-'}
+
+    set_palette(ncolors=len(ALGOS))
+
+    # ------------------------ plot the data
+
+    for d, dataset in enumerate(DATASETS):
+        axes_row = axes[d]
+        for b, nbytes in enumerate(NBYTES_LIST):
+            ax = axes_row[b]
+            if fake_data:  # TODO handle real data
+                data_tmp = data * (.5 + nbytes / 64.)  # slightly less
+            assert np.max(data_tmp) <= 1.
+            for algo in ALGOS:
+                # x = np.log10(Rs).astype(np.int32)
+                x = Rs
+                sb.tsplot(data=data_tmp, condition=ALGOS, time=x, ax=ax, n_boot=100,
+                          ls=line_styles_for_nbytes[nbytes])
+
+    # ------------------------ legend
+
+    ax = axes.ravel()[-1]
+    leg_lines, leg_labels = ax.get_legend_handles_labels()
+    # for some reason, each algo appears 3x, so just take first
+    leg_lines, leg_labels = leg_lines[:len(ALGOS)], leg_labels[:len(ALGOS)]
+
+    plt.figlegend(leg_lines, leg_labels, loc='lower center',
+                  ncol=len(ALGOS), labelspacing=0)
+
+    # ------------------------ axis cleanup / formatting
+
+    # configure all axes
+    for i, ax_row in enumerate(axes):
+        for j, ax in enumerate(ax_row):
+            title = "{}, {}B".format(DATASETS[i], NBYTES_LIST[j])
+            ax.set_title(title, y=1.01)
+            ax.set_ylim([0, 1])
+            # ax.set_xlim([0, 3 + .1])
+            # ax.set_xlim([1, np.max(Rs) * 1.1])
+            ax.set_xscale("log")
+
+            # remove all legends except the very last one
+            if i != len(axes) or j != len(ax_row):
+                ax.legend_.remove()
+
+    # remove x labels except for bottom axis
+    for ax in axes[:-1, :].ravel():
+        plt.setp(ax.get_xticklabels(), visible=False)
+        # ax.get_xaxis().set_visible(False)
+
+    if axes.shape[1] > 1:
+        # hide y axis for axes not in left col
+        for i, ax in enumerate(axes[:, 1:].ravel()):
+            # pass
+            # ax.get_yaxis().set_visible(False)
+            ax.get_yaxis().set_ticklabels([], labelpad=-10, fontsize=1)
+
+        # ylabel left col
+        for i, ax in enumerate(axes[:, 0].ravel()):
+            ax.set_ylabel("Recall@R")
+
+        # xlabel bottom rows
+        for i, ax in enumerate(axes[-1, :].ravel()):
+            # no idea why we need the dummy tick at the beginning
+            ax.set_xticklabels(['', '0', '1', '2', ''])
+            # ax.set_xticklabels(['', '0', '1', '2', '3'])
+
+        axes[-1, -1].set_xticklabels(['', '0', '1', '2', '3'])
+        axes[-1, 1].set_xlabel("Log10(R)")
+
+    # ------------------------ show / save plot
+
+    # plt.tight_layout(h_pad=.02, w_pad=.02)
+    plt.tight_layout(w_pad=.02)
+    # plt.subplots_adjust(top=.88, bottom=.21, hspace=.4)
+    plt.suptitle(suptitle, fontsize=16)
+    plt.subplots_adjust(top=.91, bottom=.11)
+    save_fig(fname)
+    # plt.show()
+
+
+def distortion_fig(data=None, suptitle=None, fname='l2_distortion'):
+    # experiment params:
+    #   datasets = Sift1M, Convnet1M, LabelMe22k, MNIST
+    #   bytes = [8, 16, 32]
+    # layout: [ndatasets x nums_bytes] (ie, [4x3])
+    #   each subplot a barplot showing corr with err bars
+
+    DATASETS = ['Sift1M', 'Convnet', 'LabelMe', 'MNIST']
+    ALGOS = ['Bolt', 'PQ', 'OPQ', 'PairQ']
+    NBYTES_LIST = [8, 16, 32]
+
+    figsize = (6, 8)
+    sb.set_context("talk", rc={'xtick.major.pad': 3})
+    set_palette(ncolors=len(ALGOS))
+    # fig, axes = plt.subplots(4, 3)
+    fig, axes = plt.subplots(4, 1, figsize=figsize)
+    axes = axes.reshape((4, 1))
+
+    if suptitle is None:
+        suptitle = 'Quality of Approximate Distances'
+
+    fake_data = data is None
+    if fake_data:
+        algo2offset = {'Bolt': .4, 'PQ': .3, 'OPQ': .45, 'PairQ': .5}
+        nfake_corrs = 10
+
+        dicts = []
+        for dataset in DATASETS:
+            for nbytes in NBYTES_LIST:
+                for algo in ALGOS:
+                    if fake_data:
+                        corrs = np.random.rand(nfake_corrs) / 2.
+                        corrs += algo2offset[algo]
+                        corrs *= .9 + .1 * nbytes / 32.
+                    params = {'algo': algo, 'dataset': dataset,
+                              'nbytes': '{}B'.format(nbytes)}
+                    dicts += [dict(params, **{'corr': c}) for c in corrs]
+
+        # data = pd.DataFrame.from_records(dicts, index=[0])
+        data = pd.DataFrame.from_records(dicts)
+        # print data
+        # return
+
+    # ------------------------ plot the data
+
+    for d, dataset in enumerate(DATASETS):
+        # df_dataset = data.loc[data['dataset'] == dataset]
+        df = data.loc[data['dataset'] == dataset]
+        df.rename(columns={'algo': ' '}, inplace=True)  # hide from legend
+
+        ax = axes.ravel()[d]
+        sb.barplot(x='nbytes', y='corr', hue=' ', data=df, ax=ax)
+
+        # for b, nbytes in enumerate(NBYTES_LIST):
+        #     ax = axes_row[b]
+        #     # df_nbytes = df_dataset.loc[data['nbytes'] == nbytes]
+        #     df = df_dataset.loc[data['nbytes'] == nbytes]
+
+            # data_tmp = df.loc[(df['algo'] == algo) * (df['algo'] == algo) ]
+
+            # assert np.max(data_tmp) <= 1.
+            # for algo in ALGOS:
+            #     df = df_nbytes.loc[data['algo'] == algo]
+                # # x = np.log10(Rs).astype(np.int32)
+                # x = Rs
+                # sb.tsplot(data=data_tmp, condition=ALGOS, time=x, ax=ax, n_boot=100,
+                #           ls=line_styles_for_nbytes[nbytes])
+
+    # ------------------------ legend
+
+    ax = axes.ravel()[-1]
+    leg_lines, leg_labels = ax.get_legend_handles_labels()
+    plt.figlegend(leg_lines, leg_labels, loc='lower center',
+                  ncol=len(ALGOS), labelspacing=0)
+
+    # ------------------------ axis cleanup / formatting
+
+    # configure all axes
+    for i, ax in enumerate(axes.ravel()):
+        title = "{}".format(DATASETS[i])
+        ax.set_title(title, y=1.01)
+        ax.set_ylim([0, 1])
+        ax.set_xlabel('', labelpad=-10)
+        ax.set_ylabel('Correlation With\nTrue Distance')
+        ax.legend_.remove()
+
+    # ------------------------ show / save plot
+
+    # plt.tight_layout()  # for fig size 6x9
+    plt.tight_layout(h_pad=.8)
+    plt.suptitle(suptitle, fontsize=16)
+    # plt.subplots_adjust(top=.92, bottom=.08)  # for fig size 6x9
+    plt.subplots_adjust(top=.90, bottom=.08)
+    # save_fig(fname)
+    plt.show()
 
 
 def main():
@@ -292,8 +500,12 @@ def main():
     # popcount_fig()
     # encoding_fig(data_enc=True)
     # encoding_fig(data_enc=False)
-    # encoding_fig()
-    query_speed_fig()
+    encoding_fig()
+    # query_speed_fig()
+    # recall_r_fig()
+    # recall_r_fig(suptitle='Nearest Neighbor Recall, Euclidean', fname='l2_recall')
+    # recall_r_fig(suptitle='Nearest Neighbor Recall, Dot Product', fname='mips_recall')
+    # distortion_fig(fname='l2_distortion')
 
 
 if __name__ == '__main__':
